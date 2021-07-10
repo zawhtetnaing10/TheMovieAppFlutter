dd8e26b40dc24097dec6e2745cc923fe6b1af337
zawhtetnaing10 <zawgyi.gog@gmail.com>
2021-07-10 13:27:22 +0630

    Getting necessary data from Model for Home Screen and adding them into StreamController

--- movie_app/lib/blocs/home_bloc.dart
+++ movie_app/lib/blocs/home_bloc.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:movie_app/data/models/movie_model.dart';
+import 'package:movie_app/data/models/movie_model_impl.dart';
 import 'package:movie_app/data/vos/actor_vo.dart';
 import 'package:movie_app/data/vos/genre_vo.dart';
 import 'package:movie_app/data/vos/movie_vo.dart';
@@ -12,6 +14,58 @@ class HomeBloc {
   StreamController<List<ActorVO>> mActorsStreamController;
   StreamController<List<MovieVO>> mShowCaseMovieListStreamController;
   StreamController<List<MovieVO>> mMoviesByGenreListStreamController;
+
+  /// Models
+  MovieModel mMovieModel = MovieModelImpl();
+
+  HomeBloc() {
+    /// Now Playing Movies Database
+    mMovieModel.getNowPlayingMoviesFromDatabase().then((movieList) {
+      mNowPlayingStreamController.sink.add(movieList);
+    }).catchError((error) {});
+
+    /// Popular Movies Database
+    mMovieModel.getPopularMoviesFromDatabase().then((movieList) {
+      mPopularMoviesListStreamController.sink.add(movieList);
+    }).catchError((error) {});
+
+    /// Genres
+    mMovieModel.getGenres().then((genreList) {
+      mGenreListStreamController.sink.add(genreList);
+
+      /// Movies By Genre
+      _getMoviesByGenreAndRefresh(genreList.first.id);
+    }).catchError((error) {});
+
+    /// Genres Database
+    mMovieModel.getGenresFromDatabase().then((genreList) {
+      mGenreListStreamController.sink.add(genreList);
+
+      /// Movies By Genre
+      _getMoviesByGenreAndRefresh(genreList.first.id);
+    }).catchError((error) {});
+
+    /// Showcases Database
+    mMovieModel.getTopRatedMoviesFromDatabase().then((movieList) {
+      mShowCaseMovieListStreamController.sink.add(movieList);
+    }).catchError((error) {});
+
+    /// Actors
+    mMovieModel.getActors(1).then((actorList) {
+      mActorsStreamController.sink.add(actorList);
+    }).catchError((error) {});
+
+    /// Actors Database
+    mMovieModel.getAllActorsFromDatabase().then((actorList) {
+      mActorsStreamController.sink.add(actorList);
+    }).catchError((error) {});
+  }
+
+  void _getMoviesByGenreAndRefresh(int genreId) {
+    mMovieModel.getMoviesByGenre(genreId).then((moviesByGenre) {
+      mMoviesByGenreListStreamController.sink.add(moviesByGenre);
+    }).catchError((error) {});
+  }
 
   void dispose() {
     mNowPlayingStreamController.close();
